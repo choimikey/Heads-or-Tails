8b3e13738595f893a1f44644a3a1fa8f0676823b
Michael Choi <132119138+choimikey@users.noreply.github.com>
2023-07-25 13:27:13 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
+#Heads or Tails
 
+import random
+import time
+
+print("This program is a random coin toss.")
+time.sleep(2)
+print("You will get heads or tails in 5 seconds")
+time.sleep(1)
+print("You will get heads or tails in 4 seconds")
+time.sleep(1)
+print("You will get heads or tails in 3 seconds")
+time.sleep(1)
+print("You will get heads or tails in 2 seconds")
+time.sleep(1)
+print("You will get heads or tails in 1 seconds")
+time.sleep(1)
+random_number = random.randint(0,1)
+if random_number == 0:
+    print("Heads.")
+else:
+    print("Tails.")
